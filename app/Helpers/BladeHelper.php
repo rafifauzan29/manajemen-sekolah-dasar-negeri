b1f2570358bf43dd61f11d0fc34aa4cc07b52f92
rafifauzan29 <rafifauzan1597@gmail.com>
2025-08-27 20:07:51 +0700

    2nd

--- app/Helpers/BladeHelper.php
+++ app/Helpers/BladeHelper.php
@@ -5,19 +5,16 @@ use Illuminate\Support\Facades\Route;
 if (!function_exists('isActiveRoute')) {
     function isActiveRoute($routes, $activeClass = 'bg-gray-200 text-gray-900 font-semibold', $inactiveClass = 'hover:bg-gray-100 hover:text-gray-800')
     {
-        // Pastikan $routes selalu array
         if (!is_array($routes)) {
             $routes = [$routes];
         }
 
-        // Cek apakah salah satu route cocok
         foreach ($routes as $route) {
             if (Route::currentRouteNamed($route)) {
                 return $activeClass;
             }
         }
 
-        // Jika tidak cocok, pakai inactive class
         return $inactiveClass;
     }
 }
